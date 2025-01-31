bf82ab8459665ef3b1b5f406f84848e37383eecc
henrikskog <henrikskog01@gmail.com>
2025-01-31 09:13:40 +0100

    Add config

---
+++ README.md
@@ -0,0 +1,3 @@
+```bash
+deno run --allow-env --allow-read --allow-write script.ts
+```
---
+++ script.ts
@@ -0,0 +1,86 @@
+import {
+  complexModifications,
+  map,
+  rule,
+  writeToProfile,
+} from "https://deno.land/x/karabinerts@1.30.3/deno.ts";
+import type {
+Modifier,
+  Rule,
+  RuleBuilder,
+} from "https://deno.land/x/karabinerts@1.30.3/index.ts";
+
+const PROFILE_SCRIPT_WRITES_TO = "Default";
+
+const HYPER_MODIFIERS: Modifier[] = ["left_command", "left_shift", "left_option", "left_control"];
+
+export const rules = () => [
+  rule("Caps Lock → Hyper").manipulators([
+    map("caps_lock")
+      .to({
+        key_code: "left_shift",
+        modifiers: ["left_command", "left_option", "left_control"]
+      })
+      .toIfAlone("escape"),
+  ]),
+
+  rule("Left arrow").manipulators([
+    map("h", "left_command").to("left_arrow"),
+  ]),
+
+  rule("Down arrow").manipulators([
+    map("j", "left_command").to("down_arrow"),
+  ]),
+
+  rule("Up arrow").manipulators([
+    map("k", "left_command").to("up_arrow"),
+  ]),
+
+  rule("Right arrow").manipulators([
+    map("l", "left_command").to("right_arrow"),
+  ]),
+
+  rule("Control tab forwards").manipulators([
+    map("k", HYPER_MODIFIERS).to({
+      key_code: "tab",
+      modifiers: ["left_control", "left_shift"]
+    }),
+  ]),
+
+  rule("Control tab backwards").manipulators([
+    map("j", HYPER_MODIFIERS).to({
+      key_code: "tab",
+      modifiers: ["left_control", "left_shift"]
+    }),
+  ]),
+
+  rule("Switch tab to right").manipulators([
+    map("l", HYPER_MODIFIERS).to({
+      key_code: "right_arrow",
+      modifiers: ["left_option", "left_command"]
+    }),
+  ]),
+
+  rule("Switch tab to left").manipulators([
+    map("h", HYPER_MODIFIERS).to({
+      key_code: "left_arrow",
+      modifiers: ["left_option", "left_command"]
+    }),
+  ]),
+];
+
+writeToProfile(PROFILE_SCRIPT_WRITES_TO, rules());
+
+// to print out raw config:
+function _printRawConfig(rules: Array<Rule | RuleBuilder>) {
+  const config = complexModifications(rules).rules.reduce(
+    (r, v) => ({
+      description: r.description
+        ? `${r.description}; ${v.description}`
+        : v.description,
+      manipulators: r.manipulators.concat(v.manipulators),
+    }),
+    { description: "", manipulators: [] }
+  );
+  console.log(JSON.stringify(config, null, 2));
+}
